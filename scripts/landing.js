5df0533381c0a9ae15f706a8124cd08552070ced
Bauer <bwann52@users.noreply.github.com>
2016-11-16 20:58:30 -0800

    cp9 forEach

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,4 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
+ 
+ var animatePoints = function(points) {
  
                  var points = document.getElementsByClassName('point');
  
@@ -14,3 +16,20 @@ var animatePoints = function() {
                 }
  
              };
+
+ window.onload = function() {
+    // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener('scroll', function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }     
+     });
+ }
+ 
+ 
